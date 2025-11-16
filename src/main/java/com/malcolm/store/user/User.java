08e9eb49367d05f9e7a300b6679a6cd5579a0304
Malcolm-G-Biz <malcolm.githuka@bizhub.com.au>
2025-11-16 17:43:01 +0300

    Add UserRepository
    - Add dependencies for spring-data-jpa and h2
    - Update application.properties to yml file
    - Update service class to user UserRepository

--- src/main/java/com/malcolm/store/user/User.java
+++ src/main/java/com/malcolm/store/user/User.java
@@ -1,10 +1,23 @@
 package com.malcolm.store.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "STO_user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
+
+	// JPA Entities require a default no-arg constructor. We could still omit this
+	// since we have lombok. We could use @NoArgsConstructor
+	public User() {
+	}
+
 }
--- src/main/java/com/malcolm/store/user/UserService.java
+++ src/main/java/com/malcolm/store/user/UserService.java
@@ -1,10 +1,8 @@
 package com.malcolm.store.user;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,41 +10,36 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
 	@Autowired
 	private ObjectMapper objectMapper;
+	private final UserRepository userRepository;
 
-	private AtomicLong currentId = new AtomicLong(0);
-	private List<User> userList = new ArrayList<>();
-
-	public List<User> createUser(User user) {
-		long newId = currentId.addAndGet(1);
-		user.setId(newId);
-		userList.add(user);
-		return userList;
+	public User createUser(User user) {
+		return userRepository.save(user);
 	}
 
 	public List<User> fetchAllUsers() {
-		return userList;
+		return userRepository.findAll();
 	}
 
 	public Optional<User> fetchUserById(Long id) {
-		if (id != null) {
-			return userList.stream().filter(u -> id.equals(u.getId())).findFirst();
-		}
-		return Optional.ofNullable(null);
+		return userRepository.findById(id);
 	}
 
 	public User patchUser(Long id, JsonNode update) {
 		// Get user by the id
-		User user = userList.stream().filter(u -> Long.valueOf(u.getId()) != null && id.equals(u.getId())).findFirst()
-				.orElse(null);
+		User user = userRepository.findById(id).orElse(null);
 
 		if (user != null) {
 			try {
 				objectMapper.readerForUpdating(user).readValue(update);
+				return userRepository.save(user);
 			} catch (IOException e) {
 				return null;
 			}
