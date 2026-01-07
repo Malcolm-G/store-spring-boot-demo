98e61ae38b46ca34036678f04c7ecb641a53d606
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-07 16:53:19 +0300

    Implement rewrite filters in yaml

--- store-microservices/gateway/src/main/java/com/malcolm/gateway/GatewayConfig.java
+++ store-microservices/gateway/src/main/java/com/malcolm/gateway/GatewayConfig.java
@@ -2,13 +2,11 @@ package com.malcolm.gateway;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class GatewayConfig {
 
-	@Bean
+//	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 
 		return builder.routes().route("product", r -> r.path("/products/**").uri("lb://product"))
