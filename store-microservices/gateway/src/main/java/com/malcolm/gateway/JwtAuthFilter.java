91021c50312122ac48219f539b997536f042ec1c
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-07 14:31:39 +0300

    Convert gateway to use lb (loadbalanced) instead of http
    - Disable JwtAuthFilter

--- store-microservices/gateway/src/main/java/com/malcolm/gateway/JwtAuthFilter.java
+++ store-microservices/gateway/src/main/java/com/malcolm/gateway/JwtAuthFilter.java
@@ -2,14 +2,13 @@ package com.malcolm.gateway;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
 import reactor.core.publisher.Mono;
 
-@Component
+// @Component
 public class JwtAuthFilter implements WebFilter {
 
 	@Override
