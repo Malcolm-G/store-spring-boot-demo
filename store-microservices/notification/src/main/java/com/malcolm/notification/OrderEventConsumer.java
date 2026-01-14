fa4e83999ccb437fbda7b239be675e2a33e9077e
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-14 14:36:27 +0300

    Transition notification service to use spring cloud streams

--- store-microservices/notification/src/main/java/com/malcolm/notification/OrderEventConsumer.java
+++ store-microservices/notification/src/main/java/com/malcolm/notification/OrderEventConsumer.java
@@ -1,22 +1,35 @@
 package com.malcolm.notification;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.malcolm.notification.payload.OrderCreatedEvent;
-import com.malcolm.notification.payload.OrderStatus;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class OrderEventConsumer {
 
-	@RabbitListener(queues = "${rabbitmq.queue.name}")
-	public void handleOrderEvent(OrderCreatedEvent orderEvent) {
-		System.out.println("Received Order Event: " + orderEvent);
-
-		Long orderId = orderEvent.getOrderId();
-		OrderStatus orderStatus = orderEvent.getOrderStatus();
-
-		System.out.println("Order ID: " + orderId);
-		System.out.println("Order Status: " + orderStatus);
+	/*
+	 * @RabbitListener(queues = "${rabbitmq.queue.name}") public void
+	 * handleOrderEvent(OrderCreatedEvent orderEvent) {
+	 * System.out.println("Received Order Event: " + orderEvent);
+	 * 
+	 * Long orderId = orderEvent.getOrderId(); OrderStatus orderStatus =
+	 * orderEvent.getOrderStatus();
+	 * 
+	 * System.out.println("Order ID: " + orderId);
+	 * System.out.println("Order Status: " + orderStatus); }
+	 */
+	
+	@Bean
+	Consumer<OrderCreatedEvent> orderCreated(){
+		return event -> {
+			log.info("Received order created event for order ID: {}", event.getOrderId());
+			log.info("Received order created event for user ID: {}", event.getUserId());
+		};
 	}
 }
