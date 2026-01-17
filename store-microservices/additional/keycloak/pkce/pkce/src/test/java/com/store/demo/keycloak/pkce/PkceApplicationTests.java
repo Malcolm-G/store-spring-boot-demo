7c28f1811fe80154bdcb492db1260d14a7354843
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-17 15:52:24 +0300

    Create a pkce resource server
    - Set up keycloak toaccept public clients (no client secret)
    - Set up postman to use pkce flow, so that it send a challenge when
    getting a code

---
+++ store-microservices/additional/keycloak/pkce/pkce/src/test/java/com/store/demo/keycloak/pkce/PkceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.demo.keycloak.pkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
