d1f32e3059d509a899e598f043549df3e4d08dc1
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-17 09:34:16 +0300

    Created a resource server and a pure client that calls it
    - Seen how resource server validates cedentials it receives using
    keycloak the auth provider
    - Used CommandLineRunner to run a command when the pure client starts

---
+++ store-microservices/additional/keycloak/resource/src/test/java/com/store/demo/keycloak/resource/ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.demo.keycloak.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
